c60d45fef7c09e42eecc2299dc662d83dc97db50
SamedYasin <127936936+SamedYasin@users.noreply.github.com>
2023-11-29 12:30:38 +0100

    Update

--- project-play-unity/Assets/Script/Movement.cs
+++ project-play-unity/Assets/Script/Movement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get input for forward and backward movement
+        float forwardInput = Input.GetAxisRaw("Vertical");
+
+        // Calculate movement direction
+        Vector3 movement = new Vector3(0f, 0f, forwardInput);
+
+        // Normalize the movement vector to ensure consistent speed in all directions
+        movement.Normalize();
+
+        // Move the object based on the calculated movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
     }
 }
