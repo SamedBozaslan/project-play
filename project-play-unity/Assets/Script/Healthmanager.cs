558875cac8c0fbb1dbec81ae2403d8472ff32014
SamedYasin <127936936+SamedYasin@users.noreply.github.com>
2023-12-20 10:09:35 +0100

    .

--- project-play-unity/Assets/Script/Healthmanager.cs
+++ project-play-unity/Assets/Script/Healthmanager.cs
@@ -1,10 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthmanager : MonoBehaviour
 {
     public int currentHealth = 50;
+    public int numOfHearts;
+
+    public Image[] hearts; 
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        } 
+    }
 
     public void TakeDamage(int damage)
     {
@@ -18,5 +47,7 @@ public class Healthmanager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    
 }
 
